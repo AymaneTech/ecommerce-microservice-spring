04c0c8208a77a464413a62edb1530fa9344fcf2c
geek <elmainiaymane03@gmail.com>
2024-12-21 15:52:00 +0100

    implement the discovery service

---
+++ services/discovery-service/src/test/java/com/wora/ecommerce/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wora.ecommerce.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
